bc449ccd05fdfa9f52a8b2cf52d9276557d5ab43
Danny O'Brien <danny@spesh.com>
2018-10-17 22:55:49 -0700

    Remove bashism.

--- examples/mblaze_much.zsh
+++ examples/mblaze_much.zsh
@@ -6,20 +6,20 @@
 # gets moved into an archive folder. Similarly, mail marked "spam" get moved to
 # a spam folder.
 
-function mnm() {
+mnm() {
     # mnm - mblaze notmuch
     # search and return a list of files, suitable for mblaze functions
     notmuch search --output=files $*
 }
 
-function ms() {
+ms() {
     # ms - mshow with tags
     # shortcut to show current email with tags appended
     mshow $*
     mtags .
 }
 
-function nn {
+nn {
     # nn - fast notmuch new
     # run notmuch and afew quietly and quickly, to respond to file changes made by mblaze
     # (my notmuch hooks get new mail, unless DONOTSYNC is set)
@@ -27,39 +27,39 @@ function nn {
     DONOTSYNC=1 afew -m tag:inbox
 }
 
-function minbox() {
+minbox() {
     # minbox - mblaze my inbox
     # find everything in the notmuch "inbox", and store it in the default sequence
     notmuch search --output=files tag:inbox | msort -r -d -U | mseq -S
 }
 
-function mtoday() {
+mtoday() {
     # mtoday - focus on today's email (I have a broad view of 'today')
     notmuch search --output=files "tag:inbox AND ( date:today OR date:yesterday )" | msort -r -d -U | mseq -S
 }
 
-function mflag() {
+mflag() {
     # mflag - toggle 'important' flag
     mbatchtag "awk '/+flagged/ { sub(/+flagged/,\"-flagged\"); print; next}; !/+flagged/ { printf \"+flagged \";print; next }' " $*
 }
 
-function mspam() {
+mspam() {
     # mark as spam (mails tagged spam get moved out of my Maildir Inbox into a spam folder by a utility called afew)
     mbatchtag "sed 's/+inbox/-inbox +spam +missedspam/'" $*
 }
 
-function march() {
+march() {
     # mark as archived (mails tagged archive get moved out of my Maildir Inbox by a utility called afew)
     mbatchtag "sed 's/+inbox/-inbox +archive/'" $*
 }
 
-function mt() {
+mt() {
     # find all in same thread as this email
     # uses notmuch's threading knowledge, not mblaze's
     mnm `notmuch search --output=threads $(mseq ${*:-.} | mid)`
 }
 
-function mbatchtag() {
+mbatchtag() {
     action=$1
     shift
     if [ -t 0 ]; then
@@ -70,7 +70,7 @@ function mbatchtag() {
     fi
 }
 
-function mcp () {
+mcp () {
     # run commands on a variety of searches (defaults to giving a count)
     while read -u9 -n line ; do
         command=`echo $line| sed 's/#.*$//'`
